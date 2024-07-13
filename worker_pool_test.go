ececeb0c2d845912959ad1be736732f448319cee
Bohdan Vlasenko <bogdanvlas2000@gmail.com>
2024-07-13 15:44:19 +0200

    feat: add table-driven tests

--- worker_pool_test.go
+++ worker_pool_test.go
@@ -1,16 +1,70 @@
 package worker_pool
 
-import "testing"
+import (
+	"flag"
+	"fmt"
+	"go.uber.org/zap/zapcore"
+	"log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"testing"
+	"time"
+)
 
 func TestWorkerPool(t *testing.T) {
-	tasks := []Task{}
-
-	for i := 0; i < 20; i++ {
-		tasks = append(tasks, Task{
-			ID: i,
-		})
+	tests := []struct {
+		taskCount      int
+		maxWorkerCount int
+	}{
+		{
+			taskCount:      2,
+			maxWorkerCount: 2,
+		},
+		{
+			taskCount:      5,
+			maxWorkerCount: 2,
+		},
+		{
+			taskCount:      2,
+			maxWorkerCount: 5,
+		},
 	}
 
-	pool := NewWorkerPool(tasks, 50)
-	pool.Start()
+	opts := &zap.Options{
+		Development: true,
+		TimeEncoder: zapcore.TimeEncoderOfLayout(time.RFC3339Nano),
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+	logger := zap.New(zap.UseFlagOptions(opts))
+
+	for i, test := range tests {
+		tasks := []Task{}
+
+		for i := 0; i < test.taskCount; i++ {
+			tasks = append(tasks, Task{
+				ID: i,
+			})
+		}
+
+		poolName := fmt.Sprintf("pool-%d", i)
+		pool, err := NewWorkerPool(tasks, test.maxWorkerCount, logger.WithName(poolName))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		timer := getTimer()
+		pool.Start()
+		duration := timer()
+
+		logger.Info("test completed", "workerPool", poolName, "duration", duration)
+		fmt.Println()
+
+	}
+}
+
+func getTimer() func() time.Duration {
+	start := time.Now()
+	return func() time.Duration {
+		return time.Since(start)
+	}
 }
